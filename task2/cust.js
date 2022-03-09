26995223be0e503672d59dd07acfe037821cf4b0
SamiraElgendy <47606988+SamiraElgendy@users.noreply.github.com>
2022-03-09 09:14:26 +0200

    task2

--- task2/cust.js
+++ task2/cust.js
@@ -1,16 +1,26 @@
-function apiGetter(apiLink){
+function apiGetter(apiLink, callback){
     fetch(apiLink)
     .then( (x) => {
         x.json()
         .then( (y) => {
-           y.arts.forEach(element => {
-               document.querySelector(".row").innerHTML+=`<div   class ='col-md-3 my-2'><div class="card p-3"><h4 class="card-title">${element.source.name}</h4><img src="${element.urlToImage}" class="card-img-top"><div class="card-body">
-               <p class="card-text">${element.content}</p> <a href="#" class="card-link">${element.url}</a>
-             </div></div></div>`
-           });
+            y.arts.forEach(element => {
+                document.querySelector(".row").innerHTML+<div   class ='col-md-3 my-2'><div class="card p-3"><h4 class="card-title">${element.source.name}</h4><img src="${element.urlToImage}" class="card-img-top"><div class="card-body">
+                <p class="card-text">${element.content}</p> <a href="#" class="card-link">${element.url}</a>
+              </div></div></div>})
         } )
-        .catch((err) => {
-            console.log(err);
+        .catch(err=>{
+            callback(false, err.message)
         })
-    }   
-    apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed")
+    }
+    )
+    .catch(e=> callback(false, e.message))    
+}
+
+apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res, err)=>{
+if(err) console.log(err)  
+else {
+res.forEach(element => {
+        console.log(element.id)
+    });
+}
+})
