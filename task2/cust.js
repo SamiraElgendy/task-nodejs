12460cf203e93692ec19aa2d471096a8bc6bec27
SamiraElgendy <47606988+SamiraElgendy@users.noreply.github.com>
2022-03-09 12:45:12 +0200

    test2

--- task2/cust.js
+++ task2/cust.js
@@ -1,26 +1,42 @@
-function apiGetter(apiLink, callback){
-    fetch(apiLink)
-    .then( (x) => {
-        x.json()
-        .then( (y) => {
-            y.arts.forEach(element => {
-                document.querySelector(".row").innerHTML+<div   class ='col-md-3 my-2'><div class="card p-3"><h4 class="card-title">${element.source.name}</h4><img src="${element.urlToImage}" class="card-img-top"><div class="card-body">
-                <p class="card-text">${element.content}</p> <a href="#" class="card-link">${element.url}</a>
-              </div></div></div>})
-        } )
-        .catch(err=>{
-            callback(false, err.message)
-        })
+//function apiGetter(apiLink, callback){
+ //   fetch(apiLink)
+  //  .then( (x) => {
+  //      x.json()
+  //      .then( (y) => {
+   //         y.articles.forEach(element => {
+   //             document.querySelector(".row").innerHTML+<div   //class ='col-md-3 my-2'><div class="card p-3"><h4 class="card-title">${element.source.name}</h4><img src="${element.urlToImage}" class="card-img-top"><div class="card-body">
+      //          <p class="card-text">${element.content}</p> <a href="#" class="card-link">${element.url}</a>
+      //        </div></div></div>})
+     //   } )
+     //   .catch(err=>{
+    //        callback(false, err.message)
+    //    })
+   // }
+  //  )
+   // .catch(e=> callback(false, e.message))    
+//}
+
+//apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res, err)=>{
+//if(err) console.log(err)  
+//else {
+//res.forEach(element => {
+//        console.log(element.id)
+ //   });
+//}
+//})
+const apiGetter = async (apiLink, callback) => {
+    try{
+        let data = await fetch(apiLink)
+        let jsonData = await data.json()
+        callback(jsonData)    
     }
-    )
-    .catch(e=> callback(false, e.message))    
+    catch(e){
+        callback(e.message)
+    }
 }
 
-apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res, err)=>{
-if(err) console.log(err)  
-else {
-res.forEach(element => {
-        console.log(element.id)
-    });
-}
+apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res)=>{
+    console.log(res)
 })
+
+
