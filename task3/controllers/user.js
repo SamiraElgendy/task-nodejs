fafbc268fbc88515226e5234ed485f9d0cfa809a
SamiraElgendy <47606988+SamiraElgendy@users.noreply.github.com>
2022-03-11 23:26:51 +0200

    task3

---
+++ task3/controllers/user.js
@@ -0,0 +1,55 @@
+const validator = require("validator")
+const chalk = require("chalk")
+const dealWithJson = require("./dealWithJson")
+const findMycustomerIndex = (customer, key, val)=>{
+    let i = customer.findIndex( customer => customer[key] == val )
+    return i
+}
+const addcustomer = (customerData) =>{
+    try{
+        if(customerData.name.length<3) throw new Error("invalid name")
+        users.push(customerData)
+        dealWithJson.writeData(customers)
+        console.log(chalk.green("customer Added"))    
+    }
+    catch(e){
+        console.log(chalk.red(e.message))
+    }
+}
+const showAll = () => {
+    try{
+        const customers = dealWithJson.readData()
+        if(customers.length==0) throw new Error("no customers yet")
+        else{
+            customers.forEach(customer=>{
+                console.log(chalk.green(`
+id: ${customer.id} - name: ${customer.name} - accNum: ${customer.accNum} - intialBalance: ${customer.intialBalance}- remainigBalance: ${customer.remainigBalance} 
+`))
+            })
+        }
+    }
+    catch(e){
+        console.log(chalk.red(e.message))
+    }
+}
+const showSingle = (customerId) => {
+    const customers = dealWithJson.readData()
+    const customer = findMycustomerIndex(customers, "id", customerId)  
+    if(user!=-1) console.log(customers[customer])
+    else console.log('not found')
+}
+const delcustomer = (customerId) => {
+    let customers = dealWithJson.readData()
+    let filtered = customers.filter(u=> u.id != customerId)
+    if(users.length == filtered.length) return console.log("not found")
+    dealWithJson.writeData(filtered)
+}
+const editcustomer = (customerId, newData) => {
+    const customers = dealWithJson.readData()
+    const i = customers.findIndex(u=> u.id==customerId)
+    if(i==-1) return console.log("not found")
+    users[i] = {id:customerId, ...newData}
+    dealWithJson.writeData(customers)
+    console.log("data edited")
+}
+module.exports = { addcustomer, showAll, showSingle, delcustomer, editcustomer }
---
+++ task3/index.js
@@ -0,0 +1,80 @@
+const user = require("./controllers/user")
+const yargs = require("yargs")
+yargs.command({
+    command:"addcustomer",
+    describe:"used for adding customer",
+    builder:{
+        name:{
+            type:String,
+            required:true
+        },
+        accNum:{
+            type:Number,
+        },
+        intialBalance:{
+            type:Number,
+        },
+        remainigBalance:{
+            type:Number,
+        }
+    },
+    handler:function(argv){
+        let customerData = {
+            name:argv.name,
+            accNum:argv.accNum,
+            ntialBalance:argv.ntialBalance,
+            remainigBalance:argv.remainigBalance,
+            id:Date.now()
+        }
+        customer.addcustomer(customerData)
+    }
+})
+yargs.command({
+    command:"showAll",
+    describe:"used for show all customer",
+    handler:function(){
+        customer.showAll()
+    }
+})
+yargs.command({
+    command:"showCustomer",
+    describe:"used for show single customer",
+    builder:{},
+    handler:function(argv){
+
+    }
+})
+yargs.command({
+    command:"dealWithcustomer",
+    describe:"used for add or withdraw customer",
+    builder:{
+        op:{
+            type: String,
+            required:true
+        },
+        customerId:{
+            required:true,
+            type:String
+        }
+    },
+    handler:function(argv){
+        if(argv.op!="withdraw"&& argv.op!="add") return console.log("invalid operation")
+        if(argv.op=="withdraw"&&argv.remainigBalance>=opvalue) return console.log("invalid operation")
+        else return customer.showAll(argv.customerId,argv.intialBalance+argv.op)
+    }
+})
+yargs.command({
+    command:"editcustomer",
+    describe:"used for edit customer",
+    builder:{
+        customerId:{},
+        newName:{},
+        newaccNum:{},
+        newintialBalance:{},
+        newremainigBalance:{}
+    },
+    handler:function(argv){
+
+    }
+})
+yargs.argv
