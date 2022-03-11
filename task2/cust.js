bb0b3892ef9f42a899a579c3a2d098573563f766
SamiraElgendy <47606988+SamiraElgendy@users.noreply.github.com>
2022-03-11 22:25:29 +0200

    task2

--- task2/cust.js
+++ task2/cust.js
@@ -1,42 +1,36 @@
-//function apiGetter(apiLink, callback){
- //   fetch(apiLink)
-  //  .then( (x) => {
-  //      x.json()
-  //      .then( (y) => {
-   //         y.articles.forEach(element => {
-   //             document.querySelector(".row").innerHTML+<div   //class ='col-md-3 my-2'><div class="card p-3"><h4 class="card-title">${element.source.name}</h4><img src="${element.urlToImage}" class="card-img-top"><div class="card-body">
-      //          <p class="card-text">${element.content}</p> <a href="#" class="card-link">${element.url}</a>
-      //        </div></div></div>})
-     //   } )
-     //   .catch(err=>{
-    //        callback(false, err.message)
-    //    })
-   // }
-  //  )
-   // .catch(e=> callback(false, e.message))    
-//}
-
-//apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res, err)=>{
-//if(err) console.log(err)  
-//else {
-//res.forEach(element => {
-//        console.log(element.id)
- //   });
-//}
-//})
-const apiGetter = async (apiLink, callback) => {
-    try{
-        let data = await fetch(apiLink)
-        let jsonData = await data.json()
-        callback(jsonData)    
+function apiGetter(apiLink, callback){
+    fetch(apiLink)
+   .then( (x) => {
+       x.json()
+        .then( (y) => {
+           y.articles.forEach(element => {document.querySelector(".card").innerHTML+ `<div class ='col-md-3 my-2'><img src="${element.urlToImage}" class="card-img-top" alt="..."><div class="card-body"><p class="card-text">${element.content}</p></div>`
+         
+      } )
+       .catch(err=>{
+          callback(false, err.message)
+       })
     }
-    catch(e){
-        callback(e.message)
-    }
+   )
+    .catch(e=> callback(false, e.message))    
 }
-
-apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res)=>{
+   )
+apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed",(res)=>{
     console.log(res)
 })
 
+}
 
+// ////async function apiGetter(apiLink, callback) {
+  //  try {
+    //    let data = await fetch(apiLink)
+   //     let jsonData = await data.json()
+    //    callback(jsonData)
+   // }
+   // catch (e) {
+     //   callback(e.message)
+   // }
+//}
+
+//apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res)=>{
+  //  console.log(res)
+//})
