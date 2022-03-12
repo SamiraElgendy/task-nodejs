8c2773d8d73bf10473d9d930113a1602547296c1
SamiraElgendy <47606988+SamiraElgendy@users.noreply.github.com>
2022-03-12 16:07:23 +0200

    task2

--- task2/cust.js
+++ task2/cust.js
@@ -1,36 +1,20 @@
-function apiGetter(apiLink, callback){
-    fetch(apiLink)
-   .then( (x) => {
-       x.json()
-        .then( (y) => {
-           y.articles.forEach(element => {document.querySelector(".card").innerHTML+ `<div class ='col-md-3 my-2'><img src="${element.urlToImage}" class="card-img-top" alt="..."><div class="card-body"><p class="card-text">${element.content}</p></div>`
-         
-      } )
-       .catch(err=>{
-          callback(false, err.message)
-       })
-    }
-   )
-    .catch(e=> callback(false, e.message))    
-}
-   )
-apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed",(res)=>{
-    console.log(res)
+async function apiGetter(apiLink, callback) {
+    try {
+       let data = await fetch(apiLink)
+        let jsonData = await data.json()
+       callback(jsonData)
+      }
+    catch (e) {
+        callback(e.message)
+    }}
+
+apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res)=>{
+   console.log(res)
 })
 
-}
-
-// ////async function apiGetter(apiLink, callback) {
-  //  try {
-    //    let data = await fetch(apiLink)
-   //     let jsonData = await data.json()
-    //    callback(jsonData)
-   // }
-   // catch (e) {
-     //   callback(e.message)
-   // }
-//}
-
-//apiGetter("https://newsapi.org/v2/top-headlines?country=eg&apiKey=a891eee7e84c41b0a31729bdeb1b03ed", (res)=>{
-  //  console.log(res)
-//})
+const wrapper = document.querySelector('#wrapper')
+const divWrapper = createMyOwnElement(wrapper, 'div', "row", null)
+articles.forEach((element =>{document.querySelector(".card").innerHTML+
+`<div class //='col-md-3 my-2'><img src="${element.urlToImage}" class="card-img-top" alt="..."><div class="card-body"><p class="card-text">${element.content}</p></div>`
+})
+)
